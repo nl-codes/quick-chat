a68d50ab2f0d05e9c9452ce0c88ca3ff11e5fede
lhn_nryn <dubmate2244@gmail.com>
2026-08-12 20:00:29 +0545

    format file

--- server/lib/db.js
+++ server/lib/db.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 
 // Function to connect to the mongodb database
 export const connectDB = async () => {
-  try {
-    mongoose.connection.on("connected", () =>
-      console.log("Database Connected")
-    );
+    try {
+        mongoose.connection.on("connected", () =>
+            console.log("Database Connected"),
+        );
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`);
-  } catch (error) {
-    console.log(error);
-  }
+        await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`);
+    } catch (error) {
+        console.log(error);
+    }
 };
